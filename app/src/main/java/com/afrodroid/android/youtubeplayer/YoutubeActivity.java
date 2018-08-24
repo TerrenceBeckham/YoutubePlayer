eafe03ca99283c47f79c03030f35e3de23d7dae4
TerrenceBeckham <terrencebeckham@icloud.com>
2018-08-24 16:43:50 +0200

    Added Youtube player to layout dynamically.

--- app/src/main/java/com/afrodroid/android/youtubeplayer/YoutubeActivity.java
+++ app/src/main/java/com/afrodroid/android/youtubeplayer/YoutubeActivity.java
@@ -1,10 +1,14 @@
 package com.afrodroid.android.youtubeplayer;
 
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 
 public class YoutubeActivity extends YouTubeBaseActivity
         implements YouTubePlayer.OnInitializedListener {
@@ -12,12 +16,28 @@ public class YoutubeActivity extends YouTubeBaseActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_youtube);
+        //setContentView(R.layout.activity_youtube);
+        //ConstraintLayout constraintLayout =  findViewById(R.id.activity_youtube);
+        ConstraintLayout layout = (ConstraintLayout) getLayoutInflater().inflate(R.layout.activity_youtube, null);
+        setContentView(layout);
+
+//This refers to this activity
+//This is how to set a button in code as opposed to in the layout.xml
+//        Button button1 = new Button(this);
+//        button1.setLayoutParams(new ConstraintLayout.LayoutParams(300, 100));
+//        button1.setText("Button added");
+//        layout.addView(button1);
+//
+        YouTubePlayerView player = new YouTubePlayerView(this);
+        player.setLayoutParams(new ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        layout.addView(player);
+
+
     }
 
     @Override
     public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
-        
+
     }
 
     @Override
